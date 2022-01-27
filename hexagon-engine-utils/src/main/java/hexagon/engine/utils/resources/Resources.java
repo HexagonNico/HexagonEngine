1a952f5f11db3433bd73ba943874ae918aba8468
Nico <nico.hex6@gmail.com>
2022-01-27 10:24:36 +0100

    Resource loading class

---
+++ hexagon-engine-utils/src/main/java/hexagon/engine/utils/resources/Resources.java
@@ -0,0 +1,33 @@
+package hexagon.engine.utils.resources;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+/**
+ * A utility class to manage the loading of resources.
+ * 
+ * @author Nico
+ */
+public final class Resources {
+	
+	/**
+	 * Loads a file as a string, joining all the lines in the file with a line separator.
+	 * 
+	 * @param filePath Path of the file to load from the resources folder starting with "/".
+	 * @return The whole content of the file as a single string.
+	 * @throws ResourceLoadingException If an IO error occurs while loading the file.
+	 */
+	public static String readAsString(String filePath) throws ResourceLoadingException {
+		try(BufferedReader reader = new BufferedReader(new InputStreamReader(Resources.class.getResourceAsStream(filePath)))) {
+			return reader.lines().collect(Collectors.joining("\n"));
+		} catch(NullPointerException e) {
+			System.out.println("Could not find file " + filePath);
+			throw new ResourceLoadingException("Exception loading resource " + filePath, e);
+		} catch(IOException e) {
+			System.out.print("IO error occurred while reading " + filePath);
+			throw new ResourceLoadingException("Exception loading resource " + filePath, e);
+		}
+	}
+}
