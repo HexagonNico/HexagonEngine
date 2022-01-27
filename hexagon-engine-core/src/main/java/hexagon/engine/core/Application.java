e9860a1df1c4d1f377f944daf817387dc70a6c5c
Nico <nico.hex6@gmail.com>
2022-01-27 11:29:20 +0100

    Catch exceptions in main

--- hexagon-engine-core/src/main/java/hexagon/engine/core/Application.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/core/Application.java
@@ -4,27 +4,33 @@ import hexagon.engine.core.rendering.TestRenderer;
 import hexagon.engine.lwjgl.Engine;
 import hexagon.engine.lwjgl.OpenGL;
 import hexagon.engine.lwjgl.Window;
+import hexagon.engine.utils.Log;
 
 public class Application {
 	
 	protected final void run() {
-		Engine.errorCallback(System.err);
-		Engine.init();
-		Engine.configure(false, true);
-		Window.makeVisible();
-		Engine.createCapabilities();
+		try {
+			Engine.errorCallback(System.err);
+			Engine.init();
+			Engine.configure(false, true);
+			Window.makeVisible();
+			Engine.createCapabilities();
 
-		TestRenderer renderer = new TestRenderer();
+			TestRenderer renderer = new TestRenderer();
 
-		while(!Window.shouldClose()) {
-			OpenGL.clearFrame(0.5f, 0.5f, 1.0f);
-			renderer.render();
-			Window.update();
+			while(!Window.shouldClose()) {
+				OpenGL.clearFrame(0.5f, 0.5f, 1.0f);
+				renderer.render();
+				Window.update();
+			}
+		} catch(Exception any) {
+			Log.error("Uncaught exception in main thread");
+			any.printStackTrace();
+		} finally {
+			OpenGL.cleanUp();
+			Window.destroy();
+			Engine.terminate();
 		}
-
-		OpenGL.cleanUp();
-		Window.destroy();
-		Engine.terminate();
 	}
 
 	public static void main(String[] args) {
