cbfcf23dff1fa15a471692868f5383ba6ace83f4
Nico <nico.hex6@gmail.com>
2022-01-29 20:35:01 +0100

    Written documentation

--- hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/GameManager.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/GameManager.java
@@ -4,30 +4,60 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Class that holds all the components that are currently in the scene and all the running systems.
+ * 
+ * @author Nico
+ */
 public final class GameManager {
 	
+	/**Table of components that uses entities and component's classes as rows and columns of a table */
 	private final HashMap<GameEntity, HashMap<Class<?>, Object>> componentsTable;
+	/**List of all running game systems */
 	private final ArrayList<GameSystem> systems;
 
+	/**
+	 * Creates game manager.
+	 */
 	public GameManager() {
 		this.componentsTable = new HashMap<>();
 		this.systems = new ArrayList<>();
 	}
 
+	/**
+	 * Creates an entity with the first free id.
+	 * 
+	 * @return The newly created game entity.
+	 */
 	public GameEntity createEntity() {
 		GameEntity entity = new GameEntity(this, this.nextId());
 		this.componentsTable.put(entity, new HashMap<>());
 		return entity;
 	}
 
+	/**
+	 * Adds a component to an entity.
+	 * 
+	 * @param entity The entity to add the component to.
+	 * @param component The component to add.
+	 */
 	public void addComponent(GameEntity entity, Object component) {
 		this.componentsTable.get(entity).put(component.getClass(), component);
 	}
 
+	/**
+	 * Adds a game system.
+	 * 
+	 * @param system The system to add.
+	 */
 	public void addSystem(GameSystem system) {
 		this.systems.add(system);
 	}
 
+	/**
+	 * Runs all systems.
+	 * Called in main game loop.
+	 */
 	public void update() {
 		this.systems.forEach(system -> {
 			system.beforeAll();
@@ -38,14 +68,33 @@ public final class GameManager {
 		});
 	}
 
+	/**
+	 * Gets a component from an entity.
+	 * 
+	 * @param <T> Type of the component.
+	 * @param entity Entity holding the component.
+	 * @param type Class of the component.
+	 * 
+	 * @return The requested component.
+	 */
 	public <T> T getComponent(GameEntity entity, Class<T> type) {
 		return type.cast(this.componentsTable.get(entity).get(type));
 	}
 
+	/**
+	 * Removes an entity from the scene.
+	 * 
+	 * @param entity The entity to remove.
+	 */
 	public void removeEntity(GameEntity entity) {
 		this.componentsTable.remove(entity);
 	}
 
+	/**
+	 * Gets the next free id to create an entity.
+	 * 
+	 * @return The first unused id.
+	 */
 	private int nextId() {
 		int id = 0;
 		List<Integer> entities = this.componentsTable.keySet().stream()
--- hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/GameSystem.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/GameSystem.java
@@ -3,21 +3,52 @@ package hexagon.engine.core.ecs;
 import java.util.Arrays;
 import java.util.Collection;
 
+/**
+ * A game system runs once every frame
+ * and updates all entities that have the right components.
+ * 
+ * @author Nico
+ */
 public abstract class GameSystem {
 	
+	/**All the components that an entity needs for this system */
 	private final Collection<Class<?>> requiredComponents;
 
+	/**
+	 * Creates a game system.
+	 * 
+	 * @param requiredComponents List of all the components an entity needs.
+	 */
 	public GameSystem(Class<?>... requiredComponents) {
 		this.requiredComponents = Arrays.asList(requiredComponents);
 	}
 
+	/**
+	 * Checks if all the required components are in the given collection.
+	 * 
+	 * @param components Collection of all of the components of an entity.
+	 * 
+	 * @return True if the given collection contains all the required components, otherwise false.
+	 */
 	public final boolean componentsMatch(Collection<Object> components) {
 		return components.stream().map(component -> component.getClass()).toList().containsAll(this.requiredComponents);
 	}
 
+	/**
+	 * Runs once per frame before any entity is processed.
+	 */
 	protected abstract void beforeAll();
 
+	/**
+	 * Runs once per every entity that has the required components.
+	 * Applies this system's function.
+	 * 
+	 * @param entity The entity that is being processed.
+	 */
 	protected abstract void process(GameEntity entity);
 
+	/**
+	 * Runs once per frame after all entities have been processed.
+	 */
 	protected abstract void afterAll();
 }
