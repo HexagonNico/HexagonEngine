6ade967666f07fd80cc72d03e7f52eab725f45cd
Nico <nico.hex6@gmail.com>
2022-02-02 10:02:27 +0100

    Fixed lighting by removing specular lighting

--- hexagon-engine-core/src/main/resources/shaders/fragment/plain_color_shader.glsl
+++ hexagon-engine-core/src/main/resources/shaders/fragment/plain_color_shader.glsl
@@ -1,12 +1,12 @@
 #version 400 core
 
 in vec3 point_light_result;
-in vec3 specular_light_result;
+//in vec3 specular_light_result;
 
 out vec4 final_color;
 
 uniform vec3 color;
 
 void main(void) {
-	final_color = vec4(color * point_light_result + specular_light_result, 1.0);
+	final_color = vec4(color * point_light_result/* + specular_light_result*/, 1.0);
 }
