6d6cfbb065a1605291d95e52ee565e10242b67be
Nico <nico.hex6@gmail.com>
2022-02-03 15:37:31 +0100

    Components no longer need to be registered

--- hexagon-engine-core/src/main/java/hexagon/engine/core/components/Transform2D.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/core/components/Transform2D.java
@@ -7,12 +7,26 @@ import hexagon.engine.math.vector.Float2;
 import hexagon.engine.math.vector.Float3;
 import hexagon.engine.utils.json.JsonObject;
 
+/**
+ * Component that holds a 2D transformation.
+ * 
+ * @author Nico
+ */
 public class Transform2D extends Component {
 	
+	/**Position in a 2D space */
 	public Float2 position;
+	/**Rotation around x and y axis */
 	public Float2 rotation;
+	/**Scale on x and y axis */
 	public Float2 scale;
 
+	/**
+	 * Constructs a transform component from a {@link JsonObject}.
+	 * Constructor used when loading the component from a json file.
+	 * 
+	 * @param jsonObject JsonObject containing the component's data.
+	 */
 	public Transform2D(JsonObject jsonObject) {
 		JsonObject positionJson = jsonObject.getObject("position").orElse(JsonObject.empty());
 		JsonObject rotationJson = jsonObject.getObject("rotation").orElse(JsonObject.empty());
@@ -28,16 +42,32 @@ public class Transform2D extends Component {
 		this.scale = new Float2(x, y);
 	}
 
+	/**
+	 * Constructs a default transform
+	 * at position (0, 0), with rotation (0, 0) and scale (1, 1).
+	 */
 	public Transform2D() {
 		this(Float2.ZERO, Float2.ZERO, Float2.ONE);
 	}
 
+	/**
+	 * Constructs a transform from the given values.
+	 * 
+	 * @param position Position in a 2D space.
+	 * @param rotation Rotation around x and y axis.
+	 * @param scale Scale on x and y axis.
+	 */
 	public Transform2D(Float2 position, Float2 rotation, Float2 scale) {
 		this.position = position;
 		this.rotation = rotation;
 		this.scale = scale;
 	}
 
+	/**
+	 * Computes the transformation matrix using position, rotation and scale in this component.
+	 * 
+	 * @return The transformation matrix.
+	 */
 	public Matrix4 transformationMatrix() {
 		return Matrices.transformation(
 			new Float3(this.position.x(), this.position.y(), 0.0f),
--- hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/SceneLoader.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/core/ecs/SceneLoader.java
@@ -1,19 +1,50 @@
 package hexagon.engine.core.ecs;
 
-import hexagon.engine.core.components.SpriteComponent;
+import java.lang.reflect.InvocationTargetException;
+
+import hexagon.engine.utils.Log;
 import hexagon.engine.utils.json.JsonArray;
 import hexagon.engine.utils.json.JsonObject;
 
+/**
+ * Utility class used to load a scene file in the game manager.
+ * A scene is nothing but a collection of entities with their components.
+ * 
+ * @author Nico
+ */
 public final class SceneLoader {
 
+	/**Class should not be instantiated */
+	private SceneLoader() {}
+
+	/**
+	 * Loads a scene from a json file.
+	 * <p>
+	 * 	If this method fails to read the file, no entity is created.
+	 * 	If a component cannot be read, it is not instantiated.
+	 * </p>
+	 * 
+	 * @param filePath Path to the scene file from the resources folder, starting with {@code / }.
+	 * @param gameManager Game manager needed to create entities.
+	 */
 	public static void loadScene(String filePath, GameManager gameManager) {
 		JsonObject sceneJson = JsonObject.fromFileOrEmpty(filePath);
 		JsonArray entitiesArray = sceneJson.getArray("entities").orElse(JsonArray.empty());
 		entitiesArray.forEachObject(entityJson -> {
 			GameEntity entity = gameManager.createEntity();
-			// TODO - From class name
-			JsonObject spriteJson = entityJson.getObject("hexagon.engine.core.components.SpriteComponent").orElse(JsonObject.empty());
-			entity.addComponent(new SpriteComponent(spriteJson));
+			entityJson.keySet().forEach(key -> {
+				try {
+					JsonObject componentJson = entityJson.getObject(key).orElse(JsonObject.empty());
+					Object component = Class.forName(key).getConstructor(JsonObject.class).newInstance(componentJson);
+					entity.addComponent((Component) component);
+				} catch (InstantiationException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					Log.error("Class " + key + " cannot be instantiated");
+				} catch (NoSuchMethodException e) {
+					Log.error("Cannot instantiate " + key + ": missing jsonObject constructor");
+				} catch (ClassNotFoundException e) {
+					Log.error("Class " + key + " does not exist");
+				}
+			});
 		});
 	}
 }
