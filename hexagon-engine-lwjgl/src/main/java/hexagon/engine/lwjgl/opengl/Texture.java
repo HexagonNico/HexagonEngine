4419db3b93bc12f1b8fd70671e88d9c0ea8e2d6b
Nico <nico.hex6@gmail.com>
2022-02-05 20:37:30 +0100

    New PNG decoder

--- hexagon-engine-lwjgl/src/main/java/hexagon/engine/lwjgl/opengl/Texture.java
+++ hexagon-engine-lwjgl/src/main/java/hexagon/engine/lwjgl/opengl/Texture.java
@@ -1,13 +1,14 @@
 package hexagon.engine.lwjgl.opengl;
 
-import java.io.IOException;
+import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+
+import javax.imageio.ImageIO;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL13;
 
-import de.matthiasmann.twl.utils.PNGDecoder;
 import hexagon.engine.lwjgl.Log;
 
 /**
@@ -70,24 +71,33 @@ public final class Texture {
 	 */
 	private static Texture loadTexture(String file) {
 		try {
-			// TODO - New PNG Decoder
-			PNGDecoder decoder = new PNGDecoder(Texture.class.getResourceAsStream(file));
-			ByteBuffer buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
-			decoder.decode(buffer, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
-			buffer.flip();
+			BufferedImage image = ImageIO.read(Texture.class.getResourceAsStream(file));
+			ByteBuffer buffer = imageToBuffer(image);
 			int id = OpenGL.createTexture();
 			GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
 			GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 1);
 			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
 			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
+			GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, image.getWidth(), image.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
 			Texture texture = new Texture(id);
 			textures.put(file, texture);
 			return texture;
-		} catch (IOException e) {
+		} catch (Exception e) {
 			Log.error("Could not load texture " + file);
 			return ERROR;
 		}
+	}
+
+	private static ByteBuffer imageToBuffer(BufferedImage image) {
+		int[] argb = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
+		ByteBuffer buffer = ByteBuffer.allocateDirect(4 * image.getWidth() * image.getHeight());
+		for(int i = 0; i < argb.length; i++) {
+			buffer.put((byte) ((argb[i] >> 16) & 0xff));
+			buffer.put((byte) ((argb[i] >> 8) & 0xff));
+			buffer.put((byte) ((argb[i]) & 0xff));
+			buffer.put((byte) ((argb[i] >> 24) & 0xff));
+		}
+		return buffer.flip();
 	}
 
 	/**
