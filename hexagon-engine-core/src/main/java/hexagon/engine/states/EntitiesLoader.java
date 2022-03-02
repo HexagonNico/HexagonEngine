a14b9889a4d27afac8590a848f9dc7fcff73f21a
Nico <nico.hex6@gmail.com>
2022-03-02 15:28:24 +0100

    Entities loader can now return an entity when loading

--- hexagon-engine-core/src/main/java/hexagon/engine/states/EntitiesLoader.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/states/EntitiesLoader.java
@@ -18,8 +18,18 @@ import hexagon.engine.utils.Log;
 import hexagon.engine.utils.json.JsonArray;
 import hexagon.engine.utils.json.JsonObject;
 
+/**
+ * Static utility class used to load entities files.
+ * <p>
+ * 	Entities files are json files that contain entities and their components
+ * 	and are used to instantiate entities in a state.
+ * </p>
+ * 
+ * @author Nico
+ */
 public final class EntitiesLoader {
 
+	/**Registry containing all components that can be instantiated */
 	private static final HashMap<String, Function<GameEntity, ? extends Component>> componentsRegistry = new HashMap<>();
 
 	static {
@@ -33,7 +43,27 @@ public final class EntitiesLoader {
 		registerComponent("hexagon.engine.ReflectivityComponent", ReflectivityComponent::new);
 	}
 
+	/**
+	 * Loads an entities file by adding all the entities the given state.
+	 * 
+	 * @param state The state in which the entities should be loaded
+	 * @param entitiesFile Path to the entities file, from the resources folder, starting with {@code /}
+	 */
 	public static void loadEntities(GameState state, String entitiesFile) {
+		loadEntities(state, entitiesFile, -1);
+	}
+
+	/**
+	 * Loads an entities file by adding all the entities the given state
+	 * and returns one of the loaded entities.
+	 * 
+	 * @param state The state in which the entities should be loaded
+	 * @param entitiesFile Path to the entities file, from the resources folder, starting with {@code /}
+	 * @param rootEntity Index of the entity to be returned
+	 * 
+	 * @return The entity with the given index or {@code null} if the index is out of bounds
+	 */
+	public static GameEntity loadEntities(GameState state, String entitiesFile, int rootEntity) {
 		JsonObject jsonObject = JsonObject.fromFileOrEmpty(entitiesFile);
 		JsonArray entitiesJsonArray = jsonObject.getArray("entities").orElse(JsonArray.empty());
 		ArrayList<GameEntity> loadedEntities = new ArrayList<>();
@@ -50,8 +80,20 @@ public final class EntitiesLoader {
 				}
 			});
 		});
+		if(rootEntity >= 0 && rootEntity < loadedEntities.size()) {
+			return loadedEntities.get(rootEntity);
+		} else {
+			return null;
+		}
 	}
 
+	/**
+	 * Registers a component.
+	 * Only registered components can be loaded from entities files.
+	 * 
+	 * @param key Key to be used in the json files to create this component
+	 * @param constructor The component's constructor passed as a method reference
+	 */
 	public static void registerComponent(String key, Function<GameEntity, ? extends Component> constructor) {
 		if(!componentsRegistry.containsKey(key)) {
 			componentsRegistry.put(key, constructor);
@@ -60,5 +102,6 @@ public final class EntitiesLoader {
 		}
 	}
 
+	/**Class should not be instantiated */
 	private EntitiesLoader() {}
 }
