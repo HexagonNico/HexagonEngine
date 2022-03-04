f1eb182ce6492216204810e7565bc186f1c3a19e
Nico <nico.hex6@gmail.com>
2022-03-04 22:51:51 +0100

    Testing camera

--- hexagon-engine-example/src/main/java/hexagon/engine/example/TestState.java
+++ hexagon-engine-example/src/main/java/hexagon/engine/example/TestState.java
@@ -18,30 +18,6 @@ public final class TestState extends GameState {
 	@Override
 	public void onStart() {
 		dragonEntity = EntitiesLoader.loadEntities(this, "/entities/dragon-orbit.json", 1);
-		/*
-		GameEntity cameraEntity = new GameEntity(this);
-		Camera cameraComponent = cameraEntity.addComponent(Camera::new);
-		cameraComponent.setPosition(new Float3(0.0f, 0.0f, 5.0f));
-
-		GameEntity dragonEntity = new GameEntity(this);
-		Transform3D dragonTransform = dragonEntity.addComponent(Transform3D::new);
-		dragonTransform.setPosition(0.0f, -4.0f, -10.0f);
-		ModelComponent dragonModel = dragonEntity.addComponent(ModelComponent::new);
-		dragonModel.model = Model.getOrLoad("/models/dragon.obj");
-		dragonModel.color = new Color(0.83f, 0.68f, 0.21f);
-		ReflectivityComponent dragonReflectivity = dragonEntity.addComponent(ReflectivityComponent::new);
-		dragonReflectivity.setReflectivity(1.0f);
-
-		GameEntity squareEntity = new GameEntity(this);
-		squareEntity.addComponent(Transform2D::new);
-		SpriteComponent squareSprite = squareEntity.addComponent(SpriteComponent::new);
-		squareSprite.setTexture("/textures/windmill.png");
-
-		GameEntity sunEntity = new GameEntity(this);
-		Transform3D sunTransform = sunEntity.addComponent(Transform3D::new);
-		sunTransform.setPosition(200.0f, 200.0f, 100.0f);
-		sunEntity.addComponent(LightComponent::new);
-		*/
 	}
 
 	@Override
