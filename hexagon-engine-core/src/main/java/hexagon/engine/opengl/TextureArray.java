0cf4848106db131715c3f20faf588136fd6cbee6
Nico <nico.hex6@gmail.com>
2022-03-08 21:15:07 +0100

    Texture arrays are now loaded from json files and are no longer created with the Builder pattern

--- hexagon-engine-core/src/main/java/hexagon/engine/opengl/TextureArray.java
+++ hexagon-engine-core/src/main/java/hexagon/engine/opengl/TextureArray.java
@@ -1,7 +1,6 @@
 package hexagon.engine.opengl;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.lwjgl.opengl.GL11;
@@ -12,10 +11,18 @@ import org.lwjgl.opengl.GL30;
 import hexagon.engine.resources.Image;
 import hexagon.engine.resources.ResourceLoadingException;
 import hexagon.engine.resources.Resources;
-import hexagon.engine.utils.Log;
+import hexagon.engine.utils.json.JsonArray;
+import hexagon.engine.utils.json.JsonObject;
 
 public final class TextureArray {
-	
+
+	private static final HashMap<String, TextureArray> textures = new HashMap<>();
+
+	public static TextureArray getOrLoad(String textureFile) {
+		TextureArray texture = textures.get(textureFile);
+		return texture != null ? texture : loadTextureArray(textureFile);
+	}
+
 	public final int id;
 
 	private TextureArray(int id) {
@@ -25,57 +32,57 @@ public final class TextureArray {
 	public void bind() {
 		// TODO - Texture unit
 		GL13.glActiveTexture(GL13.GL_TEXTURE0);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, id);
+		GL11.glBindTexture(GL30.GL_TEXTURE_2D_ARRAY, this.id);
 	}
 
-	public static Builder ofSize(int width, int height) {
-		return new Builder(width, height);
-	}
+	private static TextureArray loadTextureArray(String file) {
+		try {
+			// Load json file
+			JsonObject textureJson = JsonObject.fromFile(file);
+			JsonObject sizeJson = textureJson.getObjectOrEmpty("size");
+			JsonArray layersArray = textureJson.getArrayOrEmpty("layers");
+			
+			// All layers have the same size
+			int width = sizeJson.getInt("width", 16);
+			int height = sizeJson.getInt("height", 16);
+			int layers = layersArray.length();
 
-	public static final class Builder {
+			// Allocate buffer
+			ByteBuffer buffer = ByteBuffer.allocateDirect(4 * width * height * layers);
+			HashMap<String, Image> loadedImages = new HashMap<>();
 
-		private final int width;
-		private final int height;
-		private final HashMap<String, Image> loadedImages;
-		private final ArrayList<Layer> layers;
+			// Load layers
+			layersArray.forEachObject(layerJson -> {
+				layerJson.getString("image").ifPresent(imagePath -> {
+					Image image = loadedImages.get(imagePath);
+					if(image == null) {
+						try {
+							image = Resources.loadImage(imagePath);
+							loadedImages.put(imagePath, image);
+						} catch (ResourceLoadingException e) {
+							// TODO - Error layer
+						}
+					}
+					int x = layerJson.getInt("x", 0);
+					int y = layerJson.getInt("x", 0);
+					image.toBuffer(buffer, x, y, width, height);
+				});
+			});
+			buffer.flip();
 
-		private Builder(int width, int height) {
-			this.width = width;
-			this.height = height;
-			this.loadedImages = new HashMap<>();
-			this.layers = new ArrayList<>();
-		}
-
-		public void withLayer(String imageFile, int offsetX, int offsetY) {
-			Image image = loadedImages.get(imageFile);
-			if(image == null) {
-				try {
-					image = Resources.loadImage(imageFile);
-					this.loadedImages.put(imageFile, image);
-				} catch (ResourceLoadingException e) {
-					Log.error("Could not load file " + imageFile);
-					// TODO - Error texture
-				}
-			}
-			this.layers.add(new Layer(offsetX, offsetY, image));
-		}
-
-		public TextureArray create() {
+			// Create texture
 			int id = OpenGL.createTexture();
-			ByteBuffer buffer = ByteBuffer.allocateDirect(4 * this.width * this.height * this.layers.size());
-			this.layers.forEach(layer -> layer.image.toBuffer(buffer, layer.offsetX, layer.offsetY, this.width, this.height));
 			GL11.glBindTexture(GL30.GL_TEXTURE_2D_ARRAY, id);
 			// glTexStorage3D(GL_TEXTURE_2D_ARRAY, mipLevelCount, GL_RGBA8, width, height, layerCount); ?
-			GL12.glTexImage3D(GL30.GL_TEXTURE_2D_ARRAY, 0, GL11.GL_RGBA, this.width, this.height, this.layers.size(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer.flip());
+			GL12.glTexImage3D(GL30.GL_TEXTURE_2D_ARRAY, 0, GL11.GL_RGBA, width, height, layers, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buffer);
 			GL11.glTexParameteri(GL30.GL_TEXTURE_2D_ARRAY, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST); // TODO - Texture parameters
 			GL11.glTexParameteri(GL30.GL_TEXTURE_2D_ARRAY, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
 			GL11.glTexParameteri(GL30.GL_TEXTURE_2D_ARRAY, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
 			GL11.glTexParameteri(GL30.GL_TEXTURE_2D_ARRAY, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
 			return new TextureArray(id);
+		} catch (ResourceLoadingException e) {
+			// TODO - Error texture array
+			return null;
 		}
-	}
-
-	private static record Layer(int offsetX, int offsetY, Image image) {
-
 	}
 }
