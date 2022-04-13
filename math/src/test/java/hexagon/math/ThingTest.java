cb9b31df27c4ac5c567bcc0dbbda751729722692
Nico <nico.hex6@gmail.com>
2022-04-13 13:44:25 +0200

    Test dependencies

---
+++ math/src/test/java/hexagon/math/ThingTest.java
@@ -0,0 +1,12 @@
+package hexagon.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ThingTest {
+
+	@Test
+	void thingTest() {
+		Assertions.assertTrue(true);
+	}
+}
